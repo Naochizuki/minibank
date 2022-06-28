86c9643070ee8998ba1992ed22fbbb31e7191588
NeoSwitchh <booklord34@gmail.com>
2022-06-28 23:37:35 +0700

    ADD transaction

--- app/Http/Controllers/TellerController.php
+++ app/Http/Controllers/TellerController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Rekening;
+use App\Models\Nasabah;
+use App\Models\Transaksi;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
 
 class TellerController extends Controller
 {
@@ -12,7 +19,17 @@ class TellerController extends Controller
     }
 
     public function showTellerTransaksi() {
-        return view('Dashboard.teller page.teller transaction');
+        $today = Carbon::now('Asia/Jakarta')->format('Y-m-d');
+        $weekStart = Carbon::now('Asia/Jakarta')->format('Y-m-d');
+        $weekEnds = Carbon::now('Asia/Jakarta')->subDay(7)->format('Y-m-d');
+
+        $nasabahs = Nasabah::get();
+        $rekenings = Rekening::get();
+        $todayIns = Transaksi::where('tgl_transaksi', $today, 1)->where('jenis_transaksi', 'Pemasukan', 1)->get();
+        $todayOuts = Transaksi::where('tgl_transaksi', $today, 1)->where('jenis_transaksi', 'Pengeluaran',1)->get();
+        $weekIns = Transaksi::where('tgl_transaksi', '>=', $weekEnds)->where('tgl_transaksi', '<=', $weekStart)->where('jenis_transaksi', 'Pemasukan')->get();
+        $weekOuts = Transaksi::where('tgl_transaksi', '>=', $weekEnds)->where('tgl_transaksi', '<=', $weekStart)->where('jenis_transaksi', 'Pengeluaran')->get();
+        return view('Dashboard.teller page.teller transaction', compact(['nasabahs', 'todayIns', 'todayOuts', 'weekIns', 'weekOuts', 'rekenings']));
     }
 
     public function showTellerMutation() {
